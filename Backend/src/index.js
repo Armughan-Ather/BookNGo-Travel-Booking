1722c807a783626a7ac446ca365fae33ab365063
MohammadShahmeerUlHaq <msuhk2003@gmail.com>
2024-10-03 01:08:56 +0500

    utils

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,4 +5,18 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+try {
+    connectDB();
+} catch (error) {
+    console.error("Connection Failed: ", error);
+    process.exit(1);
+}
+
+app.on("error", (error) => {
+    console.error("ERR: ", error);
+    throw error;
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Running at PORT: ${process.env.PORT}`);
+});
