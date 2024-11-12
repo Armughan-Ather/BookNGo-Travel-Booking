9f43afe05fb9809d23567069f0dc1b04f3f6488c
MohammadShahmeerUlHaq <msuhk2003@gmail.com>
2024-11-12 20:23:50 +0500

    removed residents and passengers, added bill in reservations

--- Backend/src/models/resident.model.js
+++
@@ -1,9 +0,0 @@
-export const createTableResident = `
-CREATE TABLE IF NOT EXISTS Resident (
-id INT AUTO_INCREMENT PRIMARY KEY,
-hotelReservationId int NOT NULL,
-name VARCHAR(50) not null,
-passportOrCnic VARCHAR(50) not null,
-FOREIGN KEY (hotelReservationId) REFERENCES HotelReservation(id)
-)
-`;
