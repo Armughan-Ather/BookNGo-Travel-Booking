4d00bda6179da26ef8b61e8fcc4ea0bbb86aafeb
Armughan-Ather <armughan.ather83@gmail.com>
2024-11-12 21:11:38 +0500

    deleted extra tables

--- Backend/src/models/passenger.model.js
+++
@@ -1,9 +0,0 @@
-export const createTablePassenger = `
-CREATE TABLE IF NOT EXISTS Passenger (
-id INT AUTO_INCREMENT PRIMARY KEY,
-flightReservationId int NOT NULL,
-name VARCHAR(50) not null,
-passportOrCnic VARCHAR(50) not null,
-FOREIGN KEY (flightReservationId) REFERENCES FlightReservation(id)
-)
-`;
