4b26ddfe2eb48bff91db5459c554e6a8c303f96d
MohammadShahmeerUlHaq <msuhk2003@gmail.com>
2024-11-16 03:34:58 +0500

    rating done

--- Backend/src/routes/airline.routes.js
+++ Backend/src/routes/airline.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { allAirlines } from "../controllers/airline.controller.js";
+import { updateAirlineRating } from "../controllers/airline.controller.js";
 
 const router = Router()
 
 router.route("/allAirlines").get(allAirlines)
+router.route("/updateAirlineRating").get(updateAirlineRating)
 
 export default router;
