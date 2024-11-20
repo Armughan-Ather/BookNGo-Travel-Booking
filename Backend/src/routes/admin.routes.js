2fe0b637b3d9d32d08d21f0b0663087ccefbbbcf
MohammadShahmeerUlHaq <msuhk2003@gmail.com>
2024-11-20 18:54:59 +0500

    bundleHIstory

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { loginAdmin } from "../controllers/admin.controller.js";
 import { updateHotelReservationStatuses } from "../controllers/admin.controller.js";
 import { updateFlightReservationStatuses } from "../controllers/admin.controller.js";
+import { updateBundleReservationStatuses } from "../controllers/admin.controller.js";
 
 const router = Router()
 
 router.route("/login").post(loginAdmin)
 router.route("/updateHotelReservationStatuses").get(updateHotelReservationStatuses)
 router.route("/updateFlightReservationStatuses").get(updateFlightReservationStatuses)
+router.route("/updateBundleReservationStatuses").get(updateBundleReservationStatuses)
 
 export default router;
