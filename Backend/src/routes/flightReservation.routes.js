bb7f6aa453845f2faafd43321e5e4692eb0b87b3
MohammadShahmeerUlHaq <msuhk2003@gmail.com>
2024-12-01 20:52:25 +0500

    /flightReservation/updateFlightReservation

--- Backend/src/routes/flightReservation.routes.js
+++ Backend/src/routes/flightReservation.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { reserveFlight } from "../controllers/flightReservation.controller.js";
+import { updateFlightReservation } from "../controllers/flightReservation.controller.js";
 
 const router = Router()
 
 router.route("/reserveFlight").post(reserveFlight)
+router.route("/updateFlightReservation").post(updateFlightReservation)
 
 export default router;
